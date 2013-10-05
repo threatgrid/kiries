28b6ac65bfe1ccb1b934437d380b176ad520c5be
Craig Brozefsky <craig@red-bean.com>
2013-10-05 13:33:36 -0500

    Use ligher theme from Kibana for docs.

--- src/kiries/layout.clj
+++ src/kiries/layout.clj
@@ -5,7 +5,7 @@
 (defn full-layout [title & content]
   (html5
    [:head
-    (include-css "/kibana/common/css/bootstrap.dark.min.css")
+    (include-css "/kibana/common/css/bootstrap.light.min.css")
     (include-css "/kibana/common/css/bootstrap-responsive.min.css")
     (include-css "/kibana/common/css/font-awesome.min.css")
     [:title title]]
