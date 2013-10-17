916020b5de910173342d856370da896efb55e4eb
Craig Brozefsky <craig@red-bean.com>
2013-10-16 23:23:26 -0500

    Added dependencies on tools we will use for integrations.

--- project.clj
+++ project.clj
@@ -3,22 +3,27 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [clj-logging-config "1.9.6"]
+                 [slingshot "0.10.3"]
                  
                  [riemann "0.2.2"]
                  [clj-time "0.6.0"]
                  [clojurewerkz/elastisch "1.2.0"]
                  [org.elasticsearch/elasticsearch "0.90.3"]
                  
+                 [metrics-clojure "1.0.1"]
+                 
+                 ;; web tools
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-devel "1.2.0"]
                  [ring/ring-jetty-adapter "1.2.0"]
-                 
-                 [clojure-csv/clojure-csv "1.3.2"]
 
-                 [metrics-clojure "1.0.1"]
+                 ;; integration tools
+                 [clj-http "0.7.7"]
+                 [cheshire "5.2.0"]
+                 [clojure-csv/clojure-csv "1.3.2"]
 
                  ]
   
