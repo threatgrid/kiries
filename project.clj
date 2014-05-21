8b9c5055bd1a5b496de3e82799cacf2420287713
Wes Brown <wesbrown18@gmail.com>
2014-05-21 16:43:44 -0400

    Update to the latest versions of packages, as this has bitrotted a bit.  Added clj-json dependency.

--- project.clj
+++ project.clj
@@ -1,34 +1,33 @@
 (defproject kiries "0.1.0-SNAPSHOT"
   :description "A bundled deployment of Kibana, Riemann, and ElasticSearch"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.1"]
-                 [clj-logging-config "1.9.6"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [clj-logging-config "1.9.10"]
+                 [clj-json "0.5.3"]
                  [slingshot "0.10.3"]
                  
-                 [riemann "0.2.2"]
-                 [clj-time "0.6.0"]
-                 [clojurewerkz/elastisch "1.2.0"]
-                 [org.elasticsearch/elasticsearch "0.90.3"]
+                 [riemann "0.2.5"]
+                 [clj-time "0.7.0"]
+                 [clojurewerkz/elastisch "1.5.0-beta3"]
+                 [org.elasticsearch/elasticsearch "1.1.1"]
                  
                  [metrics-clojure "1.0.1"]
                  
                  ;; web tools
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.4"]
+                 [compojure "1.1.8"]
+                 [hiccup "1.0.5"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-devel "1.2.0"]
                  [ring/ring-jetty-adapter "1.2.0"]
 
                  ;; integration tools
-                 [clj-http "0.7.7"]
-                 [cheshire "5.2.0"]
-                 [clojure-csv/clojure-csv "1.3.2"]
+                 [clj-http "0.9.1"]
+                 [cheshire "5.3.1"]
+                 [clojure-csv/clojure-csv "2.0.1"]
 
                  ;; Redis integration
-                 [com.taoensso/carmine "2.4.0"]
-                 
-                 ]
+                 [com.taoensso/carmine "2.6.2"]]
   
   :resource-paths ["resources"]
   ;;:jar-exclusions [#"^config"]
