1842a5ff4498517019b0b90b16ceebff86213988
Wes Brown <wesbrown18@gmail.com>
2014-05-21 16:49:27 -0400

    Update the elasticsearch HQ link to the latest.

--- src/kiries/web.clj
+++ src/kiries/web.clj
@@ -38,7 +38,7 @@
   (route/resources "/kibana/" {:root "htdocs/kibana"})
 
   ;; Point to our bundled installation of ElasticSearch HQ
-  (route/resources "/HQ/" {:root "htdocs/royrusso-elasticsearch-HQ-c321806"})
+  (route/resources "/HQ/" {:root "htdocs/royrusso-elasticsearch-HQ-ea630c8"})
 
 
   (route/not-found "<h1>Page not found</h1>"))
