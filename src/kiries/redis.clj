5f72722009e6301731d980c5b390c3710d919f7b
Craig Brozefsky <craig@red-bean.com>
2014-06-16 09:45:31 -0500

    Redis publush support

--- src/kiries/redis.clj
+++ src/kiries/redis.clj
@@ -4,20 +4,28 @@
             [clj-json.core :as json]))
 
 (defn publish
+  "Returns a function which publishes a single event as JSON, to the
+  spcified redis server and key."
+  [conn key]
+  (let [f (spublish conn key)]
+    (fn [e]
+      (f [e]))))
+
+(defn spublish
   "Returns a function which takes a sequence of events or other
-   values, and publishes them as JSON, to the specific redis server
-   and key.  Suitable for using in a riemann stream."
+   values, and publishes them as JSON, to the specified redis server
+   and key.  Suitable for using inside of rollup."
   [conn key]
   (fn [events]
     (let [jevents
-          (doall (remove nil?
-                         (map
-                          (fn [e]
-                            (try (json/generate-string e)
-                                 (catch Exception err
-                                   (error "Could not coerce event to json (" e "):" err)
-                                   nil)))
-                          events)))]
+          (remove nil?
+                  (map
+                   (fn [e]
+                     (try (json/generate-string e)
+                          (catch Exception err
+                            (error "Could not coerce event to json (" e "):" err)
+                            nil)))
+                   events))]
       (doseq [je jevents]
         (try
           (car/wcar conn
